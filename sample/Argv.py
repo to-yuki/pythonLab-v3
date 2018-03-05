2433fa5ee004f064f34f1b829aa34b7608f77994
to-yuki <to-yuki@jtp.co.jp>
2018-03-05 10:54:24 +0900

    commit

--- sample/Argv.py
+++ sample/Argv.py
@@ -2,9 +2,9 @@ import sys
  
 param = sys.argv
  
-print(param[0])
-print(param[1])
-print(param[2])
-print(param[3]) 
+print(param[0],end=' ')
+print(param[1],end=' ')
+print(param[2],end=' ')
+print(param[3],end=' ') 
 # print(sys.argv[3])
 
