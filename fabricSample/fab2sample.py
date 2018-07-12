6f6b9e5e3b521f4d5ffef2b932e1f2c917e5acc0
to-yuki <to-yuki@jtp.co.jp>
2018-07-12 11:27:56 +0900

    commit

--- fabricSample/fab2sample.py
+++ fabricSample/fab2sample.py
@@ -6,12 +6,23 @@ from invoke import task # @task annotation
 from invoke import Responder 
 
 #con = Connection("student@192.168.19.128", connect_kwargs = { "key_filename": "id_rsa"})
+# SSH の設定ファイル指定
+#
+Config.ssh_config_path = "ssh_config"
+
 hosts=('192.168.19.128','192.168.19.129')
-user='student'
 
 # SingleThread Run
 print('--- SingleThread Run ---')
 for host in hosts:
-  con = Connection(host,user=user,connect_kwargs = {"key_filename": "id_rsa"})
+  con = Connection(host)
   print(con.host)
   con.run("hostname") 
+
+# SingleThread Group Run
+print('--- SingleThread Group Run ---')
+result = SerialGroup('192.168.19.128','192.168.19.129').run('hostname')
+
+# MultiThread Group Run
+print('--- MultiThread Group Run ---')
+result = ThreadingGroup('192.168.19.128','192.168.19.129').run('hostname')
