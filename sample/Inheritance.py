cf782466e9631ddd1cf0c0d23a29eff30893ec6d
to-yuki <to-yuki@jtp.co.jp>
2018-07-18 18:29:28 +0900

    commit

--- sample/Inheritance.py
+++ sample/Inheritance.py
@@ -1,6 +1,5 @@
 # Class Defin.
-
-class Employee(object):  # Super Class
+class Employee:  # Super Class
 	
 	__money=0
 
@@ -10,10 +9,9 @@ class Employee(object):  # Super Class
 	def pay(self):
 		print('Total Salary Pay --> ' +str(int(self.__money)))
 
-
 class Manager(Employee):  # Class Inheritance(Sub Class)
 	def base(self,base):
-		super(Manager,self).base(base*1.5) # Super Class call method.
+		super().base(base*1.5) # Super Class call method.
 
 objs = [Employee(),Manager()]
 
