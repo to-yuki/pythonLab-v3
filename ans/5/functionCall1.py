ccb3ad49addd9d7b91c4ed7240f4079d79a65ed4
to-yuki <to-yuki@jtp.co.jp>
2018-07-30 14:23:40 +0900

    commit

---
+++ ans/5/functionCall1.py
@@ -0,0 +1,7 @@
+day = "Today"
+weather = "rain"
+def display():
+    print(day + "'s weather is " + weather + ".")
+
+# display関数を呼び出すコードを記述してください。
+display()
---
+++ lab/5/functionCall1.py
@@ -0,0 +1,6 @@
+day = "Today"
+weather = "rain"
+def display():
+    print(day + "'s weather is " + weather + ".")
+
+# display関数を呼び出すコードを記述してください。
