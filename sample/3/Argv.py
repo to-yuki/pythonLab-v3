6d260d7d338579d4e0c6adce18540d1a79662346
ToshiyukiOonishi <toshiyuki@IwashiMacBookAir.local>
2019-04-16 11:48:39 +0900

    commit

--- sample/3/Argv.py
+++ sample/3/Argv.py
@@ -1,10 +1,12 @@
 import sys
  
 param = sys.argv
- 
+
+print() 
 print(param[0],end=' ')
 print(param[1],end=' ')
 print(param[2],end=' ')
-print(param[3],end=' ') 
+print(param[3],end=' ')
+print() 
 # print(sys.argv[3])
 
