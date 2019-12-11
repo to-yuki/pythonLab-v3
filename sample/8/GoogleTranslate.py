8e2480e9b1a23c0faff78b4957c715f34e9c53c9
ToshiyukiOonishi <toshiyuki@IwashiMacBookAir.local>
2019-12-11 10:02:06 +0900

    commit

--- sample/8/GoogleTranslate.py
+++ sample/8/GoogleTranslate.py
@@ -2,10 +2,10 @@ from googletrans import Translator
 
 translator = Translator()
 
-# 日本語 -> 英語
+# 英語 -> 日本語
 result = translator.translate(text='Automate your work with Python.', dest='ja')
 print(result.text)
 
-# 英語 -> 日本語
+# 日本語 -> 英語
 result = translator.translate(text='Pythonで作業を自動化しましょう。', dest='en')
 print(result.text)
