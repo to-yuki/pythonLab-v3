abfde0eb566274d18aba50c7ce569299f7981656
to-yuki <to-yuki@jtp.co.jp>
2022-07-05 14:01:01 +0900

    Update FileEncode.py
    code cp932 change

--- sample/5/FileEncode.py
+++ sample/5/FileEncode.py
@@ -3,5 +3,5 @@ import sys,codecs
 with codecs.open('JISfile.txt','w','ISO-2022-JP') as f:
     f.write('こんにちは') # ISO-2022-JP(JIS)で書き込む
 
-with codecs.open('JISfile.txt','r','ISO-2022-JP') as f:
+with codecs.open('JISfile.txt','r','cp932') as f:
     print(f.readline()) # 文字エンコーディングが異なるため文字化け
